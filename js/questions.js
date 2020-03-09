4c2ff2bbc73aa500c9fdc42fc4b5d18f0dca2712
Ben <46476247+hexagonatron@users.noreply.github.com>
2020-03-09 21:49:15 +1030

    Page structure, added some questions

---
+++ js/questions.js
@@ -0,0 +1,98 @@
+const questionArray = [
+    {
+        name: "Question 1",
+        question: "This is a test question. What is the answer?",
+        choices: [
+            {
+                option: 1,
+                choiceText: "This is choice 1"
+            },
+            {
+                option: 2,
+                choiceText:"This is another choice"
+            },
+            {
+                option: 3,
+                choiceText: "This is the third choice"
+            },
+            {
+                option: 4,
+                choiceText: "Maybe this is the right answer"
+            }
+        ],
+        correctAnswer: 1,
+        points: 10
+    },
+    {
+        name: "Question 2",
+        question: "What is the square root of 2049?",
+        choices: [
+            {
+                option: 1,
+                choiceText: "45.265881"
+            },
+            {
+                option: 2,
+                choiceText:"42"
+            },
+            {
+                option: 3,
+                choiceText: "Foo"
+            },
+            {
+                option: 4,
+                choiceText: "Bar"
+            }
+        ],
+        correctAnswer: 1,
+        points: 10
+    },
+    {
+        name: "",
+        question: "",
+        choices: [
+            {
+                option: 1,
+                choiceText: "This is choice 1"
+            },
+            {
+                option: 2,
+                choiceText:"This is another choice"
+            },
+            {
+                option: 3,
+                choiceText: "This is the third choice"
+            },
+            {
+                option: 4,
+                choiceText: "Maybe this is the right answer"
+            }
+        ],
+        correctAnswer: 1,
+        points: 10
+    },
+    {
+        name: "What is 2+2?",
+        question: "",
+        choices: [
+            {
+                option: 1,
+                choiceText: "1"
+            },
+            {
+                option: 2,
+                choiceText:"Potato"
+            },
+            {
+                option: 3,
+                choiceText: "5 (for very large values of 2)"
+            },
+            {
+                option: 4,
+                choiceText: "4"
+            }
+        ],
+        correctAnswer: 4,
+        points: 10
+    },
+];
