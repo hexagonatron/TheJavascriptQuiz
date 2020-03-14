63477c0cf5174e5a070decfa9564f9a47e637435
Ben <46476247+hexagonatron@users.noreply.github.com>
2020-03-14 18:15:05 +1030

    Most of JS finished

--- js/script.js
+++ js/script.js
@@ -13,15 +13,42 @@ var startQuizBtn = document.querySelector(".start-quiz-btn");
 
 //Setting initial game variables
 var quizRunning = false;
+var acceptingAnswers = false;
 var score = 0;
 var currQuestionIndex = 0;
 var totalQuestions;
 var currQuestion;
 
+//Button handler fn
+buttonHandler = (event) => {
+
+    if (acceptingAnswers) {
+        acceptingAnswers = false;
+
+        var userChoice = +event.target.dataset.option;
+
+        console.log(userChoice);
+        if (userChoice === currQuestion.correctAnswer) {
+
+            score = score + currQuestion.points;
+
+        } else {
+            quizTimer.penalty(10);
+            //Button red
+            //highlight right answer
+        }
+
+        fillNextQuestion();
+
+    }
+}
+
 //Function to fill page elements from question array
 fillNextQuestion = () => {
-    if(currQuestionIndex >= totalQuestions){
+    if (currQuestionIndex >= totalQuestions) {
         //if trying to advance past questions available trigger endgame
+
+        console.log("Finished!");
     } else {
 
         //hoist current question object to global scope
@@ -32,33 +59,48 @@ fillNextQuestion = () => {
         questionText.innerText = currQuestion.question;
 
         //remove previous buttons
-        while(questionAnswers.childElementCount){
+        while (questionAnswers.childElementCount) {
             questionAnswers.removeChild(questionAnswers.childNodes[0]);
         }
 
+        //init el array
         var choiceButArray = [];
 
+        //for each choice, create corresponding button
         currQuestion.choices.forEach(choice => {
+            //container el
             var buttonDivEl = document.createElement("div");
-            buttonDivEl.classList.add("col-12","col-sm-6","mb-2","px-1");
+            buttonDivEl.classList.add("col-12", "col-sm-6", "mb-2", "px-1");
 
+            //button el
             var buttonEl = document.createElement("button");
-            buttonEl.classList.add("btn","btn-primary","col-12");   
+            buttonEl.classList.add("btn", "btn-primary", "col-12");
+
+            //add on click handler
+            buttonEl.addEventListener("click", buttonHandler);
+
+            //Encodes choiceID to specific button   
             buttonEl.dataset.option = choice.option;
             buttonEl.innerText = choice.choiceText;
 
+            //place button inside container
             buttonDivEl.appendChild(buttonEl);
 
+            //push el to Array
             choiceButArray.push(buttonDivEl);
         });
 
-        while(choiceButArray.length > 0){
+        //randomly add an el to the page
+        while (choiceButArray.length > 0) {
             var randIndex = Math.floor(Math.random() * choiceButArray.length);
             questionAnswers.appendChild(choiceButArray[randIndex]);
             choiceButArray.splice(randIndex, 1);
         }
 
+        
+        acceptingAnswers = true;
 
+        //Increment curr question for next call
         currQuestionIndex++;
     }
 }
@@ -69,31 +111,38 @@ var quizTimer = {
     currSecs: 60,
     increment: 1000,
     timerRunning: false,
-    start: function(){
+    start: function () {
         this.timerRunning = true;
-        var intervalFn = setInterval(()=> {
-            if(this.timerRunning){
-                this.currSecs -= 1;
-                this.display();
-                if(this.currSecs <= 0){
+        var intervalFn = setInterval(() => {
+            if (this.timerRunning) {
+                if (this.currSecs <= 0) {
                     this.timerRunning = false;
+                    //Trigger endgame
                 }
+                this.display();
+                this.currSecs -= 1;
             } else {
                 clearInterval(intervalFn);
             }
         }, this.increment)
     },
-    stop: function(){
+    stop: function () {
         this.timerRunning = false;
     },
-    reset: function(){
+    reset: function () {
         this.currSecs = this.startSecs;
         this.display();
     },
-    display: function(){
+    display: function () {
         timerSeconds.innerText = this.currSecs;
+    },
+    penalty: function (penalty) {
+        this.currSecs = this.currSecs >= penalty
+            ? this.currSecs - penalty
+            : 0;
+        this.display();
     }
-}   
+}
 
 //Attaching event listeners to buttons
 startQuizBtn.addEventListener("click", (event) => {
@@ -110,5 +159,5 @@ startQuizBtn.addEventListener("click", (event) => {
 
     fillNextQuestion();
     duringQuizEl.classList.remove("hidden");
-    
+
 });
